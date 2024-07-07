701dd4cda15257805d70f1cdb03de9272ace2bad
Malikov-1999 <168428731+Malikov-1999@users.noreply.github.com>
2024-07-07 21:42:44 +0300

    Добавлено

---
+++ domain/Store/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Store;
+
+public class Product
+{
+    public int Id { get; }
+    public string SKU { get; }
+    public string? Name { get; }
+   
+    public Product(int id, string sku)
+    {
+        Id = id;
+        SKU = sku;
+    }
+
+}
---
+++ infrastructure/Store.Memory/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class ProductRepository : IProductRepository
+    {
+
+        private readonly Product[] products = new[]
+        {
+            new Product(1, "001"),
+            new Product(2, "001"),
+            new Product(3, "001"),
+        };
+
+
+
+        public Product[] GetAllBySKU(string sku)
+        {
+            return products.Where(product =>  product.SKU.Contains(sku))
+                .ToArray();
+        }
+        
+    }
+}
